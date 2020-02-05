ecf71be0aa1f8366da18be5a4386372d9c9999c1
David Nguyen <hungson215@gmail.com>
2020-02-04 21:22:19 -0500

    add upcoming event route

--- routes/upcomingEvent.js
+++ routes/upcomingEvent.js
@@ -19,14 +19,26 @@ router.post('/', (req, res) => {
             return upcomingEvent.create({
                 date: req.body.date,
                 event: req.body.event
-            }, (err) => {
+            }, (err, upcomingEvent) => {
                 if(err)
                     return res.status(500).send('Internal Server Error: Unable to create upcoming events');
-                return res.status(201).send('Event created!');    
+                return res.status(201).send({message: 'Event created!', id: upcomingEvent._id});    
             });
         })
     }
     return res.status(403).send('Unauthorized Access');
 });
 
+router.get('/:id', (req, res) => verifyToken(req.headers['authorization'], (err) => {
+    if(err) {
+        if(err === 'invalid token')
+            return res.status(403).send('Unauthorized Access');
+        return res.status(500).send('Internal Server Error: Unable to verify token');    
+    }   
+    return upcomingEvent.findById(req.params.id, (err, event) => {
+        if(err)
+            return res.status(500).send('Internal Server Error: Unable to find any event');
+        return res.status(200).send(event);    
+    }) ;
+}))
 module.exports = router;
--- utils/authUtils.js
+++ utils/authUtils.js
@@ -1,4 +1,10 @@
 const jwt = require('jsonwebtoken');
-const verifyToken = (token, callback) => jwt.verify(token, process.env.TOKEN_DECODE_KEY, {algorithms:['RS512']}, (err, decoded) => callback(err, decoded));
+const verifyToken = (token, callback) => {
+    if(token) {
+        token = token.replace('Bearer ', '');
+        return jwt.verify(token, process.env.TOKEN_DECODE_KEY, {algorithms:['RS512']}, (err, decoded) => callback(err, decoded))
+    }
+    return callback('invalid token', null);
+}
 const createToken = (payload) => jwt.sign(payload, process.env.TOKEN_ENCODE_KEY, {algorithm:'RS512', expiresIn: 86400});
 module.exports = {createToken, verifyToken};

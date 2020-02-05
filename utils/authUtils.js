62b162ba221c97c5910ba9ec56a9aec6b1e9166f
David Nguyen <hungson215@gmail.com>
2020-02-04 20:19:18 -0500

    fix verifyToken

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -1,4 +1,4 @@
 const jwt = require('jsonwebtoken');
-const verifyToken = (token, callback) => jwt.verify(token, process.env.TOKEN_DECODE_KEY, {algorithms:['RS512']}, callback(err, decode));
+const verifyToken = (token, callback) => jwt.verify(token, process.env.TOKEN_DECODE_KEY, {algorithms:['RS512']}, (err, decoded) => callback(err, decoded));
 const createToken = (payload) => jwt.sign(payload, process.env.TOKEN_ENCODE_KEY, {algorithm:'RS512', expiresIn: 86400});
 module.exports = {createToken, verifyToken};
